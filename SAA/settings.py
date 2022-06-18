b4efa000315f269ec9dcbf638756dc3599cb1311
TALON <kingabdalndwan@gmail.com>
2022-06-18 15:47:21 +0300

    second commit

--- SAA/settings.py
+++ SAA/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AUTH_USER_MODEL = 'account.Profile'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'activity.apps.ActivityConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(AbstractUser):
+    # Holds the user's bio
+    bio = models.CharField(max_length=300, blank=True, null=True)
+
