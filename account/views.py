01bd6be315553c22ab2f9d67ba04ba4ef16adf02
TALON <kingabdalndwan@gmail.com>
2022-06-23 15:56:12 +0300

    "third commit"

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article, Comment
+from .forms import CommentForm
 
 
 def base(request):
@@ -14,6 +15,21 @@ def article_details(request, article_id):
     comments = Comment.objects.filter(article=article)
     context = {"article": article, "comments": comments}
     return render(request, 'article_details.html', context)
+
+
+def save_comment(request, article_id):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.article = get_object_or_404(Article, pk=article_id)
+            new_form.save()
+    else:
+        form = CommentForm()
+    context = {"form": form}
+    return render(request, "add_comment.html", context)
+
+
 
 def no_page_found(request, exception):
     return render(request, 'no_page_found.html')
